c078370a284c21cba68c2d1ee7b7c6e6791e325e
Jhericoh Janquill Lumaque <jjnlumaque@iskolarngbayan.pup.edu.ph>
2021-06-07 01:37:14 +0800

    added artist and artist top tracks feture

---
+++ Controllers/ArtistsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+using Spotify.Helpers;
+using Spotify.Models;
+
+namespace Spotify.Controllers
+{
+    [RoutePrefix("Artists")]
+    public class ArtistsController : BaseApiController
+    {
+        [HttpGet]
+        public IHttpActionResult GetArtist(int userId, string artistName)
+        {
+            try
+            {
+                using (var artist = new ArtistEntities())
+                {
+                    return UserExist(userId) ? Ok(artist.QueryArtistTopSongs(artistName).ToList()) :
+                        Content(System.Net.HttpStatusCode.NotFound, new { Message = $"User with user id: {userId} is not found on the database" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
